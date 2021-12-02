20d8758b085d3e7e6b5e1b4a7e462130e1ac961f
Martin2c51 <42279489+Martin2c51@users.noreply.github.com>
2021-12-01 22:04:08 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/v2s4sf3ef1e6nyf/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+# export_file_url = 'https://www.dropbox.com/s/v2s4sf3ef1e6nyf/export.pkl?raw=1'
+# export_file_name = 'export.pkl'
 
 path = Path(__file__).parent
 
@@ -19,28 +19,29 @@ app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Reques
 app.mount('/static', StaticFiles(directory='app/static'))
 
 
-async def download_file(url, dest):
-    if dest.exists(): return
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.read()
-            with open(dest, 'wb') as f:
-                f.write(data)
+# async def download_file(url, dest):
+#     if dest.exists(): return
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url) as response:
+#             data = await response.read()
+#             with open(dest, 'wb') as f:
+#                 f.write(data)
 
 
-async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
-    try:
-        learn = load_learner(path, export_file_name)
-        return learn
-    except RuntimeError as e:
-        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
-            print(e)
-            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
-            raise RuntimeError(message)
-        else:
-            raise
+# async def setup_learner():
+#     await download_file(export_file_url, path / export_file_name)
+#     try:
+#         learn = load_learner(path, export_file_name)
+#         return learn
+#     except RuntimeError as e:
+#         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+#             print(e)
+#             message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+#             raise RuntimeError(message)
+#         else:
+#             raise
 
+learn = load_learner("models/export.pkl")
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
